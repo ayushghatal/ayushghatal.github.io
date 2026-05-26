d806c5e663994df7f7858516807869fb0ae59175
Ayush Singh Ghatal <ayushghatal8@gmail.com>
2026-05-27 00:04:21 +0530

    tryna fix the highlighter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,4 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap(), mdx(), react()],
-  site: "https://ayushghatal.github.io",
-  base: "ayushghatal",
 });
